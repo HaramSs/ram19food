5eac2d4f46f442efdf53749914e5e864834e186c
HaramSs <haram8274@gmail.com>
2024-09-12 17:02:25 +0900

    push

--- src/ram19food/main.py
+++ src/ram19food/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from typing import Union
 import pickle
+import time
 
 app = FastAPI()
 
@@ -13,4 +14,6 @@ def read_root():
 def food(name:str):
     # 시간을 구함
     # 음식 이름과 시간을 CSV 로 저장 -> code/data.food.csv
-    return {"food": name, "time": "2024-09-14 11:12:13"}
+    my_time = time.strftime("%Y-%m-%d %H:%M:%S")
+    #"2024-09-14 11:12:13"
+    return {"food" : name, "time" : f"{my_time}"}
